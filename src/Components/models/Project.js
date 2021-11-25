2bb5f79c77cf9f18f7185577fffa770b1e3fc1a1
Diego Sastoque <diegosastque@IVANs-Air.rootstack.net>
2021-11-25 14:21:16 -0500

    add project

--- src/Components/models/Project.js
+++ src/Components/models/Project.js
@@ -1,37 +1,41 @@
-import React, { Component } from 'react'
-import "../../Utilities/css/Projects.css";
+import React, { Component } from 'react';
+import '../../Utilities/css/Projects.css';
 
 export default class Project extends Component {
-    render() {
-        return (
-            <div>
-                <div className="container-page-project" style={ { 'backgroundColor': this.props.color }}>
-                    
-                    <div className="project_image">
-                        <img src={this.props.image} alt="imagen" ></img>
-                    </div>
-                    
-                    <div className="project_description">
-                         <p className="titulo">{this.props.titulo}</p>
-                           
-                         <p className="description">
-                                {this.props.tecnologia}
-                        </p>
+  render() {
+    return (
+      <div>
+        <div
+          className='container-page-project'
+          style={{ backgroundColor: this.props.color }}
+        >
+          <div className='project_image'>
+            <img src={this.props.image} alt='imagen'></img>
+          </div>
 
-                        <p className="description">
-                        {this.props.description}
-                        </p>
-                            <ul className="description">
-                                <li><span>Project:</span>{this.props.titulo}</li>
-                                <li><span>Role:</span>{this.props.titulo}</li>
-                                <li><span>Demo</span><a href={this.props.link}>{this.props.titulo}</a></li>
-                                
-                            </ul>
-                        
-                    </div>
-                    
-                </div>
-            </div>
-        )
-    }
+          <div className='project_description'>
+            <p className='titulo'>{this.props.titulo}</p>
+
+            <p className='description'>{this.props.tecnologia}</p>
+
+            <p className='description'>{this.props.description}</p>
+            <ul className='description'>
+              <li>
+                <span>Project:</span>
+                {this.props.titulo}
+              </li>
+              <li>
+                <span>Role:</span>
+                {this.props.role}
+              </li>
+              <li>
+                <span>Demo</span>
+                <a href={this.props.link}>{this.props.titulo}</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- src/Components/pages/Projectpage.js
+++ src/Components/pages/Projectpage.js
@@ -1,63 +1,80 @@
 import React, { Component } from 'react';
 import Project from '../models/Project';
-import doctors from "../../Utilities/img/doctorsathome2.png";
-import simpleCompany from "../../Utilities/img/simplecompanypro.jpg";
-import facturacion from "../../Utilities/img/app.jpg";
+import doctors from '../../Utilities/img/doctorsathome2.png';
+import simpleCompany from '../../Utilities/img/simplecompanypro.jpg';
+import facturacion from '../../Utilities/img/app.jpg';
+import waterWell from '../../Utilities/img/waterwell.webp';
 import { withTranslation } from 'react-i18next';
 
-
 class Projectpage extends Component {
-    render() {
-      const { t } = this.props;
-        const projects = [
-          
-            {
-              title: "Doctors at Home",
-              image: doctors,
-              descripcion: t("Desarrollo de la aplicación móvil híbrida y sistema de administración."),
-              tecnologia: 'React Native | Node js | Php | HTML | CSS',
-              link: "https://www.doctorsathome.com/",
-              color: "#fff"
-            },
-            {
-              title: "Simple Company",
-              image: simpleCompany,
-              descripcion: t("Pagina para  un grupo de apasionados emprendedores, programadores y diseñadores"),
-              tecnologia: 'Wordpress | js | Google Analytics',
-              link: "https://simplecompany.io/",
-              color: "#fff"
-            },
-            {
-              title: "Bill A&e",
-              image: facturacion,
-              descripcion:
-                t("Bill A&E es una facturacion virtual para almacenamiento de datos y reportes mensuales"),
-              tecnologia: 'Node js | Materialize | HTML | CSS',
-              client: "Bills A&E",
-              link: "https://productsae.herokuapp.com/",
-              color: "#fff"
-              
-            }
-          ];
-        
-        return (
-            <div>
+  render() {
+    const { t } = this.props;
+    const projects = [
+      {
+        title: 'Water Well Online',
+        image: waterWell,
+        descripcion: t(
+          'Desarrollo de la aplicación web h y sistema de administración de pozos.'
+        ),
+        tecnologia: 'Vue js | Looback js | Bootstrap | HTML | CSS',
+        link: 'https://www.waterwellonline.com/',
+        color: '#fff',
+        role: 'Full Stack Developer',
+      },
+
+      {
+        title: 'Doctors at Home',
+        image: doctors,
+        descripcion: t(
+          'Desarrollo de la aplicación móvil híbrida y sistema de administración.'
+        ),
+        tecnologia: 'React Native | Node js | Php | HTML | CSS',
+        link: 'https://www.doctorsathome.com/',
+        color: '#fff',
+        role: 'Full Stack Developer',
+      },
+      {
+        title: 'Simple Company',
+        image: simpleCompany,
+        descripcion: t(
+          'Pagina para  un grupo de apasionados emprendedores, programadores y diseñadores'
+        ),
+        tecnologia: 'Wordpress | js | Google Analytics',
+        link: 'https://simplecompany.io/',
+        color: '#fff',
+        role: 'Full Stack Developer',
+      },
+      {
+        title: 'Bill A&e',
+        image: facturacion,
+        descripcion: t(
+          'Bill A&E es una facturacion virtual para almacenamiento de datos y reportes mensuales'
+        ),
+        tecnologia: 'Node js | Materialize | HTML | CSS',
+        client: 'Bills A&E',
+        link: 'https://productsae.herokuapp.com/',
+        color: '#fff',
+        role: 'Full Stack Developer',
+      },
+    ];
+
+    return (
+      <div>
         {projects.map((project, i) => (
-            <Project
-              key={i}
-              titulo={project.title}
-              tecnologia={project.tecnologia}
-              image={project.image}
-              description={project.descripcion}
-              color={project.color}
-              link={project.link}
-              
-            />
-          ))}
-            </div>
-        )
-    }
+          <Project
+            key={i}
+            titulo={project.title}
+            tecnologia={project.tecnologia}
+            image={project.image}
+            description={project.descripcion}
+            color={project.color}
+            link={project.link}
+            role={project.role}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
-
-export default withTranslation() (Projectpage)
+export default withTranslation()(Projectpage);
